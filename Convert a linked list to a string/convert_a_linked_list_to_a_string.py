9d49f0a788123a346fc0a5bd8755e274b0b294fa
Veritas1814 <149301025+Veritas1814@users.noreply.github.com>
2024-04-08 18:25:32 +0300

    correct file

--- Convert a linked list to a string/convert_a_linked_list_to_a_string.py
+++ Convert a linked list to a string/convert_a_linked_list_to_a_string.py
@@ -1,41 +1,17 @@
-from collections import deque
-class FrontMiddleBackQueue:
-
-    def __init__(self):
-        self.data=deque()
-
-    def pushFront(self, val: int) -> None:
-        self.data.appendleft(val)
-
-    def pushMiddle(self, val: int) -> None:
-        self.data.insert(len(self.data)//2,val)
-
-    def pushBack(self, val: int) -> None:
-        self.data.append(val)
-
-    def popFront(self) -> int:
-        return self.data.popleft()
-
-
-    def popMiddle(self) -> int:
-        self.data.rotate(len(self.data)//2)
-        a=self.data.pop()
-        self.data.rotate(-len(self.data)//2)
-        return a
-
-    def popBack(self) ->    int:
-        pass
-
-
-# Your FrontMiddleBackQueue object will be instantiated and called as such:
-obj = FrontMiddleBackQueue()
-obj.pushFront(5)
-obj.pushBack(1)
-print(obj.data)
-obj.pushMiddle(12)
-print(obj.data)
-obj.pushBack(1)
-print(obj.popMiddle())
-# param_4 = obj.popFront()
-# param_5 = obj.popMiddle()
-# param_6 = obj.popBack()
+class Node():
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+    def __repr__(self):
+        return f'{self.data}->{repr(self.next)}'
+def stringify(node):
+    if node is None:
+        return 'None' 
+    all_data=[]
+    while node.next:
+        all_data.append(str(node.data))
+        node =node.next
+    if node.next is None:
+        all_data.append(str(node.data))
+        all_data.append("None")
+        return " -> ".join(all_data)
